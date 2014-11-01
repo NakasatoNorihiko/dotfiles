0830c30531c428a50ffe005415b8f4f81f121a73
Norihiko Nakasato <n.norihiko1229@gmail.com>
2014-11-01 15:31:22 +0900

    python flymake setting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -195,4 +195,30 @@
 (add-hook 'emacs-lisp-mode-hook
 	  '(lambda ()
 	     (setq indent-tabs-mode nil))) ;;インデントは空白で行う
-;
+;; Python用flymakeの設定
+;;(install-elisp "https://github.com/illusori/emacs-flymake-cursor/flymake-cursor.el")
+;; python-mode をロードする
+(when (autoload 'python-mode "python-mode" "Python editing mode." t)
+  ;; python-mode のときのみ python-pep8 のキーバインドを有効にする
+  (setq python-mode-hook
+  (function (lambda ()
+    (local-set-key "\C-c\ p" 'python-pep8))))
+  (setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
+  (setq interpreter-mode-alist (cons '("python" . python-mode)
+                                     interpreter-mode-alist)))
+
+;; flymake for python
+(add-hook 'python-mode-hook 'flymake-find-file-hook)
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      ;; 昨日紹介した自作のシェルスクリプトを指定する
+      ;; 各々の環境にあわせて編集してください
+      (list "~/projects/dotfiles/.emacs.d/pyck.sh"  (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+(load-library "flymake-cursor.el")
---
+++ .emacs.d/pyck.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+setup_environment() {
+    # Python のインストールパスを指定する
+    PYTHON_PATH=/opt/python/current
+    IGNORE_ERRORS=E221,E701,E202
+}
+
+main() {
+    setup_environment
+    which $PYTHON_PATH/bin/pyflakes > /dev/null || exit 254
+    which $PYTHON_PATH/bin/pep8 > /dev/null || exit 254
+    $PYTHON_PATH/bin/pyflakes $*
+    $PYTHON_PATH/bin/pep8 --ignore=$IGNORE_ERRORS --repeat $*
+    exit 0
+}
+
+main $*
